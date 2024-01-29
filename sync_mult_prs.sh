99fd2fbc449f51c8c003805778339363a08a6216
Grace Cai <qqzczy@126.com>
2024-01-29 17:33:28 +0800

    Update sync_mult_prs.sh
    Signed-off-by: Grace Cai <qqzczy@126.com>

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -13,16 +13,17 @@ cd "$SCRIPT_DIR"
 
 # Define the PRs to sync.
 # The PRs will be synced in the order of the following statements.
-./sync_pr.sh preview/pingcap/docs/"$DOCS_PR"
-./sync_pr.sh preview/pingcap/docs-cn/"$DOCS_CN_PR"
-./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
-./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
+# ./sync_pr.sh preview/pingcap/docs/"$DOCS_PR"
+./sync_pr.sh preview/pingcap/docs-cn/16164
+./sync_pr.sh preview/pingcap/docs-cn/16165
+# ./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
+# ./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
 
 # Synchronize the content from master to release-x.y directories.
-rsync -av markdown-pages/zh/tidb/master/ markdown-pages/zh/tidb/"$RELEASE_DIR"/
-rsync -av markdown-pages/en/tidb/master/ markdown-pages/en/tidb/"$RELEASE_DIR"/
-rsync -av markdown-pages/en/tidb-in-kubernetes/master/ markdown-pages/en/tidb-in-kubernetes/"$RELEASE_DIR"/
-rsync -av markdown-pages/zh/tidb-in-kubernetes/master/ markdown-pages/zh/tidb-in-kubernetes/"$RELEASE_DIR"/
+# rsync -av markdown-pages/zh/tidb/master/ markdown-pages/zh/tidb/"$RELEASE_DIR"/
+# rsync -av markdown-pages/en/tidb/master/ markdown-pages/en/tidb/"$RELEASE_DIR"/
+# rsync -av markdown-pages/en/tidb-in-kubernetes/master/ markdown-pages/en/tidb-in-kubernetes/"$RELEASE_DIR"/
+# rsync -av markdown-pages/zh/tidb-in-kubernetes/master/ markdown-pages/zh/tidb-in-kubernetes/"$RELEASE_DIR"/
 
 commit_changes() {
   # Exit if TEST is set and not empty.
