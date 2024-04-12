51946f8a990451485e0c87cc85ec5e0a73021c9c
Grace Cai <qqzczy@126.com>
2024-04-12 08:59:17 +0800

    preview-6.5.9
    Signed-off-by: Grace Cai <qqzczy@126.com>

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -14,7 +14,8 @@ cd "$SCRIPT_DIR"
 # Define the PRs to sync.
 # The PRs will be synced in the order of the following statements. 
 # ./sync_pr.sh preview/pingcap/docs/"$DOCS_PR"
-./sync_pr.sh preview/pingcap/docs-cn/16740
+./sync_pr.sh preview/pingcap/docs-cn/16980
+./sync_pr.sh preview/pingcap/docs/17007
 ./sync_pr.sh preview-cloud/pingcap/docs/17056
 # ./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
 # ./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
