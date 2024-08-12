19dc74e4bdefc0ce6b5c1779fd817dc0e23cadf5
Grace Cai <qqzczy@126.com>
2024-08-12 14:28:24 +0800

    Update sync_mult_prs.sh
    Signed-off-by: Grace Cai <qqzczy@126.com>

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -14,8 +14,8 @@ cd "$SCRIPT_DIR"
 # Define the PRs to sync. 
 # The PRs will be synced in the order of the following statements. 
 # ./sync_pr.sh preview/pingcap/docs/"$DOCS_PR"
-./sync_pr.sh preview/pingcap/docs-cn/18232
-./sync_pr.sh preview/pingcap/docs/18442/
+./sync_pr.sh preview/pingcap/docs-cn/18278
+./sync_pr.sh preview/pingcap/docs/18537
 # ./sync_pr.sh preview-cloud/pingcap/docs/17468
 # ./sync_pr.sh preview-cloud/pingcap/docs/"$CLOUD_DOCS_PR"
 # ./sync_pr.sh preview-operator/pingcap/docs-tidb-operator/"$OPERATOR_DOCS_PR"
