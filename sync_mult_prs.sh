2c2cd2321af0d925b65386cd1907d6747b658943
Grace Cai <qqzczy@126.com>
2025-06-11 16:38:27 +0800

    Update sync_mult_prs.sh
    Signed-off-by: Grace Cai <qqzczy@126.com>

--- sync_mult_prs.sh
+++ sync_mult_prs.sh
@@ -18,7 +18,7 @@ cd "$SCRIPT_DIR"
 ./sync_pr.sh preview/pingcap/docs/20934
 # ./sync_pr.sh preview/pingcap/docs-cn/19140
 # ./sync_pr.sh preview/pingcap/docs/19471
-./sync_pr.sh preview-cloud/pingcap/docs/21088
+./sync_pr.sh preview-cloud/pingcap/docs/21130
 # ./sync_pr.sh preview-cloud/pingcap/docs/19895
 # ./sync_pr.sh preview-cloud/pingcap/docs/19461
 # ./sync_pr.sh preview-cloud/pingcap/docs/20303
