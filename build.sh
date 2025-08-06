1df0228f7927db28a5d1b65e5cf8718418cace06
Grace Cai <qqzczy@126.com>
2025-08-06 18:54:00 +0800

    Update build.sh
    Signed-off-by: Grace Cai <qqzczy@126.com>

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ if [ ! -e website-docs/.git ]; then
     rm -rf website-docs
   fi
   # Clone the pingcap/website-docs repository.
-  git clone --single-branch --branch feat-multi-toc https://github.com/website-docs/website-docs
+  git clone --single-branch --branch feat-multi-toc https://github.com/pingcap/website-docs
   # git clone https://github.com/pingcap/website-docs
 fi
 
