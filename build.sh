041ab011b52fe92b540eb3cc9473375f41abaf8c
Grace Cai <qqzczy@126.com>
2026-06-12 11:01:09 +0800

    Update build.sh
    Signed-off-by: Grace Cai <qqzczy@126.com>

--- build.sh
+++ build.sh
@@ -52,7 +52,8 @@ if [ ! -e website-docs/.git ]; then
     rm -rf website-docs
   fi
   # Clone the pingcap/website-docs repository.
-  git clone --single-branch --branch master https://github.com/pingcap/website-docs
+  # git clone --single-branch --branch master https://github.com/pingcap/website-docs
+  git clone --single-branch --branch qiancai:add-search-filters https://github.com/qiancai/website-docs
 fi
 
 # Create a symlink to markdown-pages in website-docs/docs.
